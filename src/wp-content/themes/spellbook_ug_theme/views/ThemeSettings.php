303ca221cd106bf300d19dc15d64e9e6b62a0d04
flobbe9 <florin735@live.com>
2024-05-19 19:20:22 +0200

    feat: add some them half finished theme settings stuff

---
+++ src/wp-content/themes/spellbook_ug_theme/views/ThemeSettings.php
@@ -0,0 +1,9 @@
+<?php
+// TODO: 
+    // store in db
+    // set input default value to db number
+    // use in customBlocks.php
+$numSliderImages;
+$numSliderImages = $_GET["numSliderImages"] ?? 7;
+
+?>
--- src/wp-content/themes/spellbook_ug_theme/views/ThemeSettingsView.php
+++ src/wp-content/themes/spellbook_ug_theme/views/ThemeSettingsView.php
@@ -1,7 +1,44 @@
-<div>
+<?php 
+require_once dirname(__DIR__, 1) ."/utils/Utils.php";
+require_once "ThemeSettings.php";
+?>
+
+
+<head>
+    <link rel="stylesheet" href="<?php echo getStyleSheetUrl("ThemeSettings.css") ?>">
+</head>
+
+<div class="themeSettingsContainer">
     <h1>Theme settings</h1>
+    <hr>
 
+    <form action="" class="hidden">
+        <!-- Hidden inputs -->
+        <div>
+            <!-- in order for form to redirect to this page -->
+            <input type="hidden" name="page" value="theme-settings">
+        </div>
 
-    <footer>v<?php echo $_ENV["VERSION"]?></footer>
+        <h2>Slider Image</h2>
+        <div>
+            <!-- Num images -->
+            <label for="numSliderImages">Max Anzahl an Bildern für ImageSlider. Betrifft alle ImageSlider auf allen Seiten!</label>
+            <br>
+            <input name="numSliderImages" type="number" min="4">
+        </div>
+            
+        <br><br>
+
+        <button class="wpButton" type="submit">
+            Submit
+        </button>
+    </form>
 </div>
 
+<footer class="footerContainer">
+    <div class="flexRight footerItemRight blueLink">
+        <a href="<?php echo $_ENV["BASE_URL"] . "/wp-admin/themes.php" ?>">
+            <?echo getSiteTitle() . " Version " . $_ENV["VERSION"]?>
+        </a>
+    </div>
+</footer>
