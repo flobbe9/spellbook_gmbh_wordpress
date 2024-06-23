f974afee995a5ea850961c78e06a642d5976d8c9
flobbe9 <florin735@live.com>
2024-06-23 19:20:35 +0200

    minor: adjust dev pipeline

--- src/wp-content/themes/spellbook_gmbh_theme/functions.php
+++ src/wp-content/themes/spellbook_gmbh_theme/functions.php
@@ -12,6 +12,7 @@ require_once __DIR__ . "/helpers/SiteMapGenerator.php";
 
 // TODO
     // auto db and updates folder backup?
+    // remove git vars once pipe works using .env
 
 // TODO
     // add english language for whole site
